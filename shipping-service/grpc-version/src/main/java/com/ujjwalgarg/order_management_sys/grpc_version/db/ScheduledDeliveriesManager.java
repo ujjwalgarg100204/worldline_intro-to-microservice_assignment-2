89076943aaa35dec29ef156799eb9c5d99b72561
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2025-01-28 12:47:44 +0530

    add mock data to shipping-service

--- shipping-service/grpc-version/src/main/java/com/ujjwalgarg/order_management_sys/grpc_version/db/ScheduledDeliveriesManager.java
+++ shipping-service/grpc-version/src/main/java/com/ujjwalgarg/order_management_sys/grpc_version/db/ScheduledDeliveriesManager.java
@@ -1,13 +1,25 @@
 package com.ujjwalgarg.order_management_sys.grpc_version.db;
 
 import com.ujjwalgarg.order_management_sys.grpc_version.entity.DeliverySchedule;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledDeliveriesManager {
+
   private final Map<String, DeliverySchedule> deliveryScheduleDB = new HashMap<>();
+
+  @PostConstruct
+  public void init() {
+    deliveryScheduleDB.put("44de4ac1-a058-4f94-aa35-9ebfbfc429d7",
+        new DeliverySchedule("44de4ac1-a058-4f94-aa35-9ebfbfc429d7", "2023-10-01"));
+    deliveryScheduleDB.put("192d589d-b98e-496b-acea-5314fff02e7a",
+        new DeliverySchedule("192d589d-b98e-496b-acea-5314fff02e7a", "2023-10-05"));
+    deliveryScheduleDB.put("f68c403e-3beb-4bc4-a2a4-32a48225a054",
+        new DeliverySchedule("f68c403e-3beb-4bc4-a2a4-32a48225a054", "2023-10-10"));
+  }
 
   public void saveDeliverySchedule(String orderId, DeliverySchedule schedule) {
     deliveryScheduleDB.put(orderId, schedule);
--- shipping-service/grpc-version/src/main/java/com/ujjwalgarg/order_management_sys/grpc_version/db/TrackingInfoManager.java
+++ shipping-service/grpc-version/src/main/java/com/ujjwalgarg/order_management_sys/grpc_version/db/TrackingInfoManager.java
@@ -1,6 +1,7 @@
 package com.ujjwalgarg.order_management_sys.grpc_version.db;
 
 import com.ujjwalgarg.order_management_sys.grpc_version.entity.ShipmentTrackingInfo;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class TrackingInfoManager {
   private final Map<String, ShipmentTrackingInfo> trackingInfoDB = new HashMap<>();
+
+  @PostConstruct
+  public void init() {
+    trackingInfoDB.put("track1", new ShipmentTrackingInfo("DELIVERED",  "2023-10-01"));
+    trackingInfoDB.put("track2", new ShipmentTrackingInfo("IN_TRANSIT",  "2023-10-05"));
+    trackingInfoDB.put("track3", new ShipmentTrackingInfo("OUT_FOR_DELIVERY",  "2023-10-10"));
+  }
 
   public void saveTrackingInfo(String trackingId, ShipmentTrackingInfo info) {
     trackingInfoDB.put(trackingId, info);
